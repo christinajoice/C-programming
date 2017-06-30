aba13e15575a25162ae90b6adaddb97fa63af01a
christinajoice <christinallrejoice@gmail.com>
2017-06-30 15:05:14 +0530

    Create five.c

---
+++ five.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b c;
+clrscr();
+printf("Enter three numbers");
+scanf("%d %d %d",&a,&b,&c);
+if(a>b)
+{
+if(a>c)
+printf("%d is greater",a);
+}
+else if(b>c)
+printf(%d is greater",b);
+else
+pritnf("%d is greater",c);
+getch();
+}
